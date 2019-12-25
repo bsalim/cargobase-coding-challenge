2dcb152696db4214451cf1e973b383dc28399838
bsalim <byonosalim@gmail.com>
2019-12-25 19:03:26 +0700

    Better models - Added tables for CrawlLogs & SearchJobs Class

--- app/search/models.py
+++ app/search/models.py
@@ -1,15 +1,11 @@
 from app import db, app
 from datetime import datetime
 
-class Movies(db.Model):
-    __tablename__ = 'movies'
-
-
 class SearchQueries(db.Model):
     __tablename__ = 'search_queries'
 
     id = db.Column(db.Integer, primary_key=True)
-
+    # job_id = db.Column(db.String(100), index=True)
     keyword = db.Column(db.String(255), nullable=True)
     result_found = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime, default=datetime.now())
@@ -29,14 +25,25 @@ class SearchResults(db.Model):
     updated_at = db.Column(db.DateTime, default=None)
 
 
+class SearchJobs(db.Model):
+    __tablename__ = 'search_jobs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    query_id = db.Column(db.Integer, index=True)
+    job_id = db.Column(db.String(50), index=True, nullable=True)
+    search_engine = db.Column(db.String(50), nullable=True)
+    is_completed = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=None)
+
+
 class CrawlLogs(db.Model):
     __tablename__ = 'crawl_logs'
 
     id = db.Column(db.Integer, primary_key=True)
     query_id = db.Column(db.Integer, index=True)
     search_engine = db.Column(db.String(50), index=True, nullable=True)
-    title = db.Column(db.String(50), index=True, nullable=True)
-    description = db.Column(db.Text, index=True, nullable=True)
-    link = db.Column(db.Text, index=True, nullable=True)
+    http_status_code = db.Column(db.String(5), nullable=True)
+    time_taken = db.Column(db.Float(10,5), default=0)
+    html = db.Column(db.Text, default=None)
     created_at = db.Column(db.DateTime, default=datetime.now())
-    updated_at = db.Column(db.DateTime, default=None)
